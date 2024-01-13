8f3e18707878a8dfb0fbbda3ee40087b3ada9a2f
Cyd0n1a <Cyd0n1a@users.noreply.github.com>
2024-01-13 21:59:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -155,5 +155,36 @@ $('.text').html(function(i, html) {
 });
 */
 /* End Rainbow Text Generator function call. */
+
+// || Dynamic Resizer. ||
+
+if (window.addEventListener) {              
+    window.addEventListener("resize", browserResize);
+} else if (window.attachEvent) {                 
+    window.attachEvent("onresize", browserResize);
+}
+var xbeforeResize = window.innerWidth;
+
+function browserResize() {
+    var afterResize = window.innerWidth;
+    if ((xbeforeResize < (970) && afterResize >= (970)) || (xbeforeResize >= (970) && afterResize < (970)) ||
+        (xbeforeResize < (728) && afterResize >= (728)) || (xbeforeResize >= (728) && afterResize < (728)) ||
+        (xbeforeResize < (468) && afterResize >= (468)) ||(xbeforeResize >= (468) && afterResize < (468))) {
+        xbeforeResize = afterResize;
+        
+    //    if (document.getElementById("adngin-try_it_leaderboard-0")) {
+    //            adngin.queue.push(function(){  adngin.cmd.startAuction(["try_it_leaderboard"]); });
+              }
+         
+    }
+    if (window.screen.availWidth <= 768) {
+      restack(window.innerHeight > window.innerWidth);
+    }
+    fixDragBtn();
+    showFrameSize();  
+  // End Dynamic Resizer.
+      
+}
+var fileID = "";
 // -------------------------------------------------------------------------
 /* EOF. EOL. MCP CORE PROTOCOLS CEASED. TRON WINS. 101010101010111111. */
