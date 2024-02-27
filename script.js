44d3f2a9d770422f3fe6e6be0baa85de354e4350
Cyd0n1a <Cyd0n1a@users.noreply.github.com>
2024-02-27 02:14:02 +0000

    Update script.js
    init template code testing.

--- script.js
+++ script.js
@@ -503,5 +503,35 @@ b.drawImage(c,0,0)
 }
 } */
 // End Protocol M22.
+
+/*
+// Test to see if the browser supports the HTML template element by checking
+// for the presence of the template element's content attribute.
+if ("content" in document.createElement("template")) {
+  // Instantiate the table with the existing HTML tbody
+  // and the row with the template
+  const tbody = document.querySelector("tbody");
+  const template = document.querySelector("#productrow");
+
+  // Clone the new row and insert it into the table
+  const clone = template.content.cloneNode(true);
+  let td = clone.querySelectorAll("td");
+  td[0].textContent = "1235646565";
+  td[1].textContent = "Stuff";
+
+  tbody.appendChild(clone);
+
+  // Clone the new row and insert it into the table
+  const clone2 = template.content.cloneNode(true);
+  td = clone2.querySelectorAll("td");
+  td[0].textContent = "0384928528";
+  td[1].textContent = "Acme Kidney Beans v2.0a";
+
+  tbody.appendChild(clone2);
+} else {
+  // Find another way to add the rows to the table because
+  // the HTML template element is not supported.
+}
+*/
 // -------------------------------------------------------------------------
 /* EOF. EOL. MCP CORE PROTOCOLS CEASED. TRON WINS..? DUN DAH DUN!! 101010101010111111. */
